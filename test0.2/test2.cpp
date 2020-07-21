2bd9239aca7e5c8931c5b08523b59673d5cfd20a
Orrawan kanking <6106021611096@fitm.kmutnb.ac.th>
2020-07-21 13:50:42 +0700

    first commit

---
+++ test0.2/test2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char  A, B;
+	cout << "1st Term fo 2020      /45132114     Wanthanee  Prachuabusupakij\n\n";
+	cout << " ----------------------------------------------------------------------------------\n";
+	cout << " | Subject  ID| Subject name                      | Score  (100) | Grad        | \n";    
+	cout << " ----------------------------------------------------------------------------------\n";
+	cout << " |  4570034   |Introduction to computer           |     85     |     A       |  \n"; 
+	cout << " |  4580247   | English                           |     74     |     B       |  \n";
+	cout << " |  4530001   | Design  Thinking                  |     92     |     A       |\n  ";
+	cout << " ----------------------------------------------------------------------------------\n";
+	cout << " |   TOTAL    | Subject = 3                       | Average = 3.87           |\n";
+	cout << "===================================================================\n\n\n";
+	cout << "KMUTNB";
+
+
+}
